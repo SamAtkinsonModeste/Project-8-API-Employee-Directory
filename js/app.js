9107914f3867f001ebb8abb3ef0be4f2a36d2d01
Samantha Atkinson <sammodeste@yahoo.com>
2022-04-08 19:32:28 +0100

    modal works - now need to get arrows working

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ function parseJSON(response) {
           let image = employee.picture.large;
           let city = employee.location.city;
           let email = employee.email;
-
+        
           employeeHTML += `
           <div class="employee" data-index="${index}">
           <div class="image"> <img src="${image}"> </div>
@@ -82,6 +82,39 @@ function parseJSON(response) {
   //SEARCH FUNCTIONALITY AUTO COMPLETE
   //------------------------------------------------------------
   //--------------------------
+  //DATALIST CLICK FUNCTION
+  //---------------------------
+ 
+
+  const clickDataList = (event) => {
+    let option = event.target;
+        if(option.tagName === "OPTION") {
+        searchInput.value = option.value;
+        dataList.style.display = 'none';
+        label.classList.add('input-active');
+        
+        
+        const detailBoxs = document.querySelectorAll('.details');
+        //  console.log(detailBoxs);
+                detailBoxs.forEach(detailBox => {
+                    const textBox = detailBox.textContent;
+                    const boxParent = detailBox.parentElement; //EMPLOYEE DIV
+
+                            if(textBox.includes(option.value)){
+                            boxParent.style.display = "flex";
+                            boxParent.classList.add('employeeData');
+                            main.classList.add('main-search');
+                            employeesGrid.id ="grid-search";
+                            
+                            
+                            } else {
+                            boxParent.style.display = "none";
+                            }
+                });
+        }
+  };
+
+  //--------------------------
   //FOCUS FUNCTION
   //---------------------------
  
@@ -89,7 +122,6 @@ function parseJSON(response) {
   const searchInputFocus = (event) => {
     const names = document.querySelectorAll('h2.name');
     let options = [];/**for my generated option elements*/
-
     //loop through each name in the h2 and insert into option elements
     names.forEach(name => {
       name = `<option class="autocomplete-list" value="${name.innerText}">${name.innerText}</option>`;
@@ -101,52 +133,34 @@ function parseJSON(response) {
     dataList.innerHTML = options.join('');
     dataList.style.display = 'block';
   
+    dataList.addEventListener("click", clickDataList);
+
     
-   
-//make sure my label stays out of the input    
-    if (dataList.style.display === 'block') {
-      label.classList.add('input-active');
-     } else {
-      dataList.style.display = 'none';
-     label.classList.remove('input-active');
+    searchInput.onblur = function () {
 
-
-}
-
-
-
-        console.log(dataList);
-        console.log(options);
-//function for clicking the option buttons and displaying the employee
-        dataList.addEventListener('click', (evt)=> {
-          let option = evt.target;
-                if(option.tagName === "OPTION") {
-                  searchInput.value = option.value;
-                  dataList.style.display = 'none';
-                 
-                  const detailBoxs = document.querySelectorAll('.details');
-                //  console.log(detailBoxs);
-               detailBoxs.forEach(detailBox => {
-                const textBox = detailBox.textContent;
+        if ( dataList.style.display === "block" && main.classList.contains("main-search")) {
+            label.classList.remove('input-active');
+            dataList.style.display = "none";
+            main.classList.remove("main-search");
+            employeesGrid.id ="employees-grid";
+            
+            let boxsDetails = document.querySelectorAll(".details");
+            boxsDetails.forEach(detailBox => {
                 const boxParent = detailBox.parentElement; //EMPLOYEE DIV
-
-                if(textBox.includes(option.value)){
                   boxParent.style.display = "flex";
-                  boxParent.classList.add('employeeData');
-                  main.classList.add('main-search');
-                  employeesGrid.id ="grid-search";
-                  
-                 
-                } else {
-                 boxParent.style.display = "none";
-                }
+                  boxParent.classList.remove('employeeData');
                });
-              }
+
+            
+        }
+
+       
+        
       
-        });
+    }
+   
+
   }; 
-  
-  
 
   //--------------------------
   //KEYUP FUNCTION
@@ -154,108 +168,128 @@ function parseJSON(response) {
  
 
 const searchInputKeyUp = (event) => {
- const optionItems = dataList.querySelectorAll('option');
- console.log(optionItems);
- searchInput = event.target.value.toLowerCase();
- const names = document.querySelectorAll('.details');
-console.log(searchInput);
- console.log(names);
-
-
- names.forEach(name => {
-   const textName = name.textContent.toLowerCase();
-   const nameBox = name.parentElement;
-
-      if(textName.includes(searchInput)) {
-        nameBox.style.display = "flex";
-        nameBox.classList.add('employeeData');
-        main.classList.add('main-search');
-        employeesGrid.id ="grid-search";
-        //controlling the datalist options
-        if(dataList.style.display === "block") {
-          optionItems.forEach(option => {
-            let parent = option;
-            let op = option.getAttribute('value');
-            op = op.toLowerCase();
-            console.log(op);
-            // let opParent = op.innerHTML;
-            console.log(parent);
-           
-            if (op.includes(searchInput)) {
-              parent.style.display = "block";
-             
-              
-            } else {
-              parent.style.display = "none";
-            }
-            
-                  
-            
-          });
-
-        }
-      } else {
-        nameBox.style.display = "none";
-       
-      }
- });
-
- dataList.addEventListener('click', (evt)=> {
-  let theOne = evt.target;
-  console.log(theOne);
-  if(theOne.tagName === "OPTION") {
-    theOne = theOne.innerText;
-  searchInput.value = theOne;
-  console.log(searchInput);
-   dataList.style.display = 'none';
-   
-  }
-   
- });  
-
-
+    searchInput = event.target.value.toLowerCase();
+    const names = document.querySelectorAll('.details');
+    const opBoxs = document.querySelectorAll("option");
   
+        names.forEach(name => {
+            const textName = name.textContent.toLowerCase();
+            const nameBox = name.parentElement;
+        
+            if(textName.includes(searchInput)) {
+                nameBox.style.display = "flex";
+                    } else {
+                    nameBox.style.display = "none";
+                    }
+        });
+
+        opBoxs.forEach(opBox => {
+            let parent = opBox;
+            let op = opBox.getAttribute("value");
+            let opText = op.toLowerCase();
+
+                if (opText.includes(searchInput)){
+                    parent.style.display = "block";
+                } else {
+                    parent.style.display = "none";
+                }
+        });
+         
+
+        dataList.addEventListener("click", clickDataList)
+          
+        searchInput.onblur = function () {
+
+            if ( dataList.style.display === "block" && main.classList.contains("main-search")) {
+                label.classList.remove('input-active');
+                dataList.style.display = "none";
+                main.classList.remove("main-search");
+                employeesGrid.id ="employees-grid";
+                
+                let boxsDetails = document.querySelectorAll(".details");
+                boxsDetails.forEach(detailBox => {
+                    const boxParent = detailBox.parentElement; //EMPLOYEE DIV
+                      boxParent.style.display = "flex";
+                      boxParent.classList.remove('employeeData');
+                   });
+    
+                
+            }
+        
+        }
+    
+       
 };
 
- //--------------------------
-  //CHANGE FUNCTION EVENT
-  //---------------------------
- 
-  const searchInputChange = (event) => {
-    searchInput.value = " ";
-        
-    if( searchInput.value === " ") {
-       main.classList.remove('main-search');
-       employeesGrid.id ="employees-grid";
-      
-      let  detailBoxs = document.querySelectorAll('.details');
-        //  console.log(detailBoxs);
-         detailBoxs.forEach(detailBox => {
-           const boxParent = detailBox.parentElement;
-           if (boxParent.classList.contains('employeeData')){
-                  boxParent.classList.remove('employeeData');
-           }
-            if (boxParent.style.display === "none") {
-              boxParent.style.display = "flex";
-              dataList.style.display = "none";
-              label.classList.remove('input-active');                
-            } 
-         });
-    }
 
-  }
+
+//---------------------------------------
+// MODAL FUNCTION
+//----------------------------------------
+function displayModal(index) {
+    // use object destructuring make our template literal cleaner
+    let { name, dob, phone, email, location: { city, street, state, postcode
+    }, picture } = employees[index];
+    let date = new Date(dob.date);
+    
+    const modalHTML = `
+    <div class="modal">
+    <button class="modal-close">X</button>
+    <div class="modal-content">
+        <div class="top-content">
+            <div class="arrow-left"></div>
+        <div class="image-modal"><img src="${picture.large}"/></div>
+        <div class="arrow-right"></div>
+       
+    </div>
+      
+    <div class="text-container">
+    <h2 class="modal-name"${name.first} ${name.last}</h2>
+    <p class="modal-email">${email}</p>
+    <p class="modal-city">${city}</p>
+    <hr />
+    <p class="modal-mobile">${phone}</p>
+    <p class="modal-address">${street.number} ${street.name}</p>
+    <p class="modal-address">${state}</p>
+    <p class="modal-address">${postcode}</p>
+    <p class="modal-birth">Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+    </div>
+    </div>
+    
+    </div>
+    `;
+
+    overlay.style.display = "block"
+    overlay.innerHTML = modalHTML;
+}
+
+  searchInput.addEventListener('focus', searchInputFocus);
+  searchInput.addEventListener('keyup', searchInputKeyUp);
 
   
+  employeesGrid.addEventListener('click', evt => {
+      if (evt.target !== employeesGrid){
 
+            const employeeBox = evt.target.closest(".employee");
+            const index = employeeBox.getAttribute("data-index");
+
+            displayModal(index);
+
+      }
+  } );
+
+
+//   function closeModal ()
+//   const modalClose = overlay.querySelector('button');
+//   console.log(modalClose);
  
 
-
-searchInput.addEventListener('keyup', searchInputKeyUp); 
-searchInput.addEventListener('focus', searchInputFocus);
-// searchInput.addEventListener('change', searchInputChange);
-
-
-
-
-
-
+  overlay.addEventListener('click', (evt) => {
+      let btn = evt.target;
+      if (btn.tagName === "BUTTON") {
+    overlay.style.display = "none";
+      }
+} );
+  
+ 
